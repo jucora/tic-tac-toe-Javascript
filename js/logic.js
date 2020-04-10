0029c8e4e8f3ded707efa251c5a1af0dca489a47
Julian <artjulius@hotmail.com>
2020-04-09 19:15:11 -0500

    main functionality working

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,10 @@
 const gameBoard = (() => {
-  const board = ["*", "*", "*", "*", "*", "*", "*", "*", "*"];
-  return board;
+  const cells = ["", "", "", "", "", "", "", "", ""];
+  return { cells };
 })();
 
-const player = (name, symbol) => {
-  return { name, symbol };
+const player = (name, character) => {
+  return { name, character };
 };
 
 const control = () => {
@@ -12,16 +12,78 @@ const control = () => {
 };
 
 //FUNCTIONS
+//
+const character = ["😁", "😎", "💩", "😝"];
+const player1 = player("Julian", character[0]);
+const player2 = player("Ana", character[1]);
+const board = gameBoard;
+let currentPlayer = player1;
+let cell = null;
+let row = null;
+let winner = null;
+let info = document.querySelector("#info");
+let game = false;
+
 function start() {
+  game = true;
+  info.textContent = `${currentPlayer.name} is playing!`;
   row = document.querySelector(".row");
-  const newBoard = gameBoard;
-  for (const cell of newBoard) {
-    let square = document.createElement("div");
-    square.textContent = cell;
-    square.classList.add("col-4", "cell");
-    row.appendChild(square);
+  for (const cell of board.cells) {
+    let squareFront = document.createElement("div");
+    squareFront.classList.add("front");
+    let squareBack = document.createElement("div");
+    squareBack.classList.add("back");
+    squareBack.setAttribute("id", "back");
+    squareFront.classList.add("col-4", "cell");
+    squareFront.setAttribute("onmousedown", "whoopie.play()");
+    row.appendChild(squareFront);
+    row.appendChild(squareBack);
+  }
+}
+
+function checkWinner(currentPlayer, index) {
+  let opt = board.cells;
+
+  console.log(currentPlayer);
+  let symbol = currentPlayer.character;
+  if (
+    (opt[0] === opt[1] && opt[1] === opt[2] && opt[0] === symbol) ||
+    (opt[3] === opt[4] && opt[4] === opt[5] && opt[3] === symbol) ||
+    (opt[6] === opt[7] && opt[7] === opt[8] && opt[6] === symbol) ||
+    (opt[0] === opt[3] && opt[3] === opt[6] && opt[0] === symbol) ||
+    (opt[1] === opt[4] && opt[4] === opt[7] && opt[1] === symbol) ||
+    (opt[2] === opt[5] && opt[5] === opt[8] && opt[2] === symbol) ||
+    (opt[0] === opt[4] && opt[4] === opt[8] && opt[0] === symbol) ||
+    (opt[2] === opt[4] && opt[4] === opt[6] && opt[2] === symbol)
+  ) {
+    info.textContent = `${currentPlayer.name} is the winner`;
+    game = false;
+  } else if (currentPlayer === player1) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
+  {
+    info.textContent = `${currentPlayer.name} is Playing!`;
   }
 }
 
 //EVENT LISTENER
 document.addEventListener("DOMContentLoaded", start());
+
+document.querySelectorAll(".cell").forEach(function (cell, index) {
+  cell.addEventListener("click", function () {
+    if (cell.textContent === "" && game) {
+      board.cells[index] = currentPlayer.character;
+      if (currentPlayer === player1) {
+        cell.textContent = currentPlayer.character;
+        checkWinner(currentPlayer);
+        currentPlayer = player2;
+      } else {
+        cell.textContent = currentPlayer.character;
+        checkWinner(currentPlayer, index);
+        currentPlayer = player1;
+      }
+    }
+  });
+});
