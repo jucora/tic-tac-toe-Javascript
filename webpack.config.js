6f89a3bc29aeb8c0241be3370f80f23713f73d6f
Julian <artjulius@hotmail.com>
2020-04-27 08:29:54 -0500

    adding eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          // eslint options (if necessary)
+        },
+      },
     ],
   },
 };
