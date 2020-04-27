0683aebc59cb7884b3c6390015acb906cf4f7366
Julian <artjulius@hotmail.com>
2020-04-27 09:20:04 -0500

    fixing import

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,26 @@
-/* eslint-disable */
-
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/js/logic.js",
+  entry: './src/js/logic.js',
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          // eslint options (if necessary)
-        },
+        loader: 'eslint-loader',
       },
     ],
   },
