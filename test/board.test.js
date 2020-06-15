c61e821ecc99ebd2e2553ffe86c5e6cfc7d127c5
jucora <artjulius@hotmail.com>
2020-06-15 15:43:52 -0500

    board test

---
+++ test/board.test.js
@@ -0,0 +1,10 @@
+import gameBoard from "../src/js/board";
+
+describe("gameBoard", () => {
+  it("should be defined", () => {
+    expect(gameBoard).toBeDefined();
+  });
+  it("should return an array of 9 empty strings", () => {
+    expect(gameBoard.cells).toEqual(["", "", "", "", "", "", "", "", ""]);
+  });
+});
--- test/testHelpers.js
+++ test/testHelpers.js
@@ -1,38 +1,30 @@
-const removeFakeDom = () => {
-  document.body.innerHTML = '';
-};
+const fakeDom = (() => {
+  const inputsDifferentValues = () => {
+    const div = document.createElement("div");
+    div.innerHTML =
+      "<h2 id='namePlayerOneTitle'></h2>" +
+      "<input id= 'playerOneName' value='Player 1 Name'></input>" +
+      "<input id= 'characterPlayerOne' value='Player 1 Character'></input>" +
+      "<h2 id='namePlayerTwoTitle'></h2>" +
+      "<input id= 'playerTwoName' value= 'Player 2 Name'></input>" +
+      "<input id= 'characterPlayerTwo' value='Player 2 Character'></input>";
+    document.body.appendChild(div);
+  };
 
-const fakeInputs = () => {
-  const div = document.createElement('div');
-  div.innerHTML = "<input id= 'playerOneName' value='Player 1 Name'></input>"
-    + "<input id= 'characterPlayerOne' value='Player 1 Character'></input>"
-    + "<input id= 'playerTwoName' value= 'Player 2 Name'></input>"
-    + "<input id= 'characterPlayerTwo' value='Player 2 Character'></input>";
-  document.body.appendChild(div);
-};
+  const emptyInputs = () => {
+    document.querySelector("#playerOneName").setAttribute("value", "");
+    document.querySelector("#playerTwoName").setAttribute("value", "");
+  };
 
-const fakeEmptyInputs = () => {
-  const div = document.createElement('div');
-  div.innerHTML = "<h2 id='namePlayerOneTitle'></h2>"
-    + "<input id= 'playerOneName' value=''></input>"
-    + "<input id= 'characterPlayerOne' value='Player 1 Character'></input>"
-    + "<h2 id='namePlayerTwoTitle'></h2>"
-    + "<input id= 'playerTwoName' value= ''></input>"
-    + "<input id= 'characterPlayerTwo' value='Player 2 Character'></input>";
-  document.body.appendChild(div);
-};
+  const inputsSameValues = () => {
+    document.querySelector("#playerOneName").setAttribute("value", "same name");
+    document.querySelector("#playerTwoName").setAttribute("value", "same name");
+  };
+  return {
+    inputsDifferentValues,
+    emptyInputs,
+    inputsSameValues,
+  };
+})();
 
-const fakeInputsSameName = () => {
-  const div = document.createElement('div');
-  div.innerHTML = "<h2 id='namePlayerOneTitle'></h2>"
-    + "<input id= 'playerOneName' value='same name'></input>"
-    + "<input id= 'characterPlayerOne' value='Player 1 Character'></input>"
-    + "<h2 id='namePlayerTwoTitle'></h2>"
-    + "<input id= 'playerTwoName' value= 'same name'></input>"
-    + "<input id= 'characterPlayerTwo' value='Player 2 Character'></input>";
-  document.body.appendChild(div);
-};
-
-export {
-  removeFakeDom, fakeInputs, fakeEmptyInputs, fakeInputsSameName,
-};
+export default fakeDom;
