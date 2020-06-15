07177ab79724318b504fe12ce450673b246fda09
jucora <artjulius@hotmail.com>
2020-06-15 15:53:48 -0500

    player test

---
+++ test/player.test.js
@@ -0,0 +1,23 @@
+import player from "../src/js/player";
+
+describe("player", () => {
+  it("should be defined", () => {
+    expect(player).toBeDefined();
+  });
+  it("should return an object with the player structure", () => {
+    const player1 = player("Julian", 1, "computer");
+    expect(player1).toEqual({
+      name: "Julian",
+      character: 1,
+      rol: "computer",
+    });
+  });
+  it("should return an object with the player structure, by default the rol should be as human", () => {
+    const player1 = player("Julian", 1);
+    expect(player1).toEqual({
+      name: "Julian",
+      character: 1,
+      rol: "human",
+    });
+  });
+});
